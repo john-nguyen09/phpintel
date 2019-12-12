530255ad15c151898a89284f01e8c7a7343f9585
John Nguyen <john.nguyen09@outlook.com>
2019-12-12 19:52:31 +1100

    Show version and goreleaser

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/john-nguyen09/phpintel/internal/jsonrpc2"
 	"github.com/john-nguyen09/phpintel/internal/lsp"
 )
 
+var (
+	flgVersion bool
+	version    string
+)
+
 func main() {
+	flag.BoolVar(&flgVersion, "version", false, "Show version of the language server")
+	flag.Parse()
+
+	if flgVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	stream := jsonrpc2.NewHeaderStream(os.Stdin, os.Stdout)
 	ctx := context.Background()
 	ctx, srv := lsp.NewServer(ctx, stream)
