a141eedbbdd7c6a9f12501517bf497b10a39b87e
John Nguyen <john.nguyen09@outlook.com>
2020-05-23 20:53:52 +1000

    Add tests for use class namespace in functions and constants

--- cases/unused/import.php
+++ cases/unused/import.php
@@ -4,14 +4,15 @@ namespace App1;
 use TestClass1;
 use TestClass2;
 use Namespace1\{
-    Test1, Test2, Test3, Test4
+    Test1, Test2, Test3, Test4, Test5
 };
 use function Namespace2\{
     function1, function2
 };
 use const Namespace3\{
     CONST1, CONST2
-}
+};
+use TestClass3;
 
 /** @var Test4 $var1 */
 $var1 = TestClass1::doSomething();
@@ -23,3 +24,6 @@ class ExtendedTest3 extends Test3
 }
 
 function1(CONST2);
+if (Test5\innerFunction()) {
+    echo 'Yay!!!' . TestClass3\SOME_CONST;
+}
